615c2538a6a3c3c4a85b799da5e38f885f1139ad
flored27 <dflores@fandm.edu>
2017-12-08 16:17:56 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,78 @@
-// Make sure to import React and ReactDOM
+// // Make sure to import React and ReactDOM
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// const BUTCHER_PRODUCTS = [
+//   'Tenderloin',
+//   'Short ribs',
+//   'Beef shin',
+//   'Ribeye'
+// ];
+//
+// // Define these exported classes
+// export class OlderCoaster extends React.Component {
+//   render () {
+//     return React.createElement('div', {className: 'oldercoaster'}, [
+//      React.createElement('p', {}, 'Two grannies having the time of their life!'),
+//      React.createElement('p', {}, 'Passengers:'),
+//      React.createElement('ul', {}, [
+//        React.createElement('li', {}, 'Agnes'),
+//        React.createElement('li', {}, 'Muriel')
+//       ])
+//     ]);
+//   }
+// }
+//
+// export class InFrontOfYou extends React.Component {
+//   react () {
+//     return React.createElement('div', {}, [
+//        React.createElement('p', {}, `You shouldn't look too far.`),
+//        React.createElement('p', {}, `Sometimes, the solution is right in front of you.`)
+//     ]);
+//   }
+// }
+//
+// export class ButcherShop extends React.Component {
+//   render () {
+//     return React.createElement('div', {className: 'butcher-shop'}, [
+//      React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
+//      React.createElement('ul', {}, [
+//       BUTCHER_PRODUCTS.map(product => React.createElement('li', {}, product))
+//       ])
+//     ]);
+//   }
+// }
+//
+// ReactDOM.render(
+//   React.createElement('div', {}, [
+//     React.createElement(OlderCoaster),
+//     React.createElement(InFrontOfYou),
+//     React.createElement(ButcherShop)
+//   ]),
+//   document.getElementById('root')
+// );
+
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Marketplace
+Explore
+ @flored27
+ Sign out
+ Watch 0
+  Star 0  Fork 2 flored27/react-components-lab-web-100817
+forked from learn-co-students/react-components-lab-web-100817
+ Code  Pull requests 0  Projects 0  Insights  Settings
+Branch: solution Find file Copy pathreact-components-lab-web-100817/src/index.js
+1194916  on Jun 2
+@Lukeghenco Lukeghenco Add solution
+1 contributor
+RawBlameHistory
+51 lines (46 sloc)  1.4 KB
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -7,10 +81,38 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ];
 
-// Define these exported classes
-export class OlderCoaster extends React.Component {};
-export class InFrontOfYou extends React.Component {};
-export class ButcherShop extends React.Component {};
+export class OlderCoaster extends React.Component {
+  render() {
+    return React.createElement('div', { className: 'oldercoaster' }, [
+      React.createElement('p', {}, 'Two grannies having the time of their life!'),
+      React.createElement('p', {}, 'Passengers:'),
+      React.createElement('ul', {}, [
+        React.createElement('li', {}, 'Agnes'),
+        React.createElement('li', {}, 'Muriel')
+      ])
+    ]);
+  }
+}
+
+export class InFrontOfYou extends React.Component {
+  render() {
+    return React.createElement('div', {}, [
+      React.createElement('p', {}, `You shouldn't look too far.`),
+      React.createElement('p', {}, 'Sometimes, the solution is right in front of you.')
+    ]);
+  }
+}
+
+export class ButcherShop extends React.Component {
+  render() {
+    return React.createElement('div', { className: 'butcher-shop' }, [
+      React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
+      React.createElement('ul', {},
+        BUTCHER_PRODUCTS.map(product => React.createElement('li', {}, product))
+      )
+    ]);
+  }
+}
 
 ReactDOM.render(
   React.createElement('div', {}, [
